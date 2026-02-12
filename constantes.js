74c27b12cb0801d53cb4d82f8cae0318b5f12504
MaicolLopez186 <maicol2005grajales@gmail.com>
2026-02-11 19:23:33 -0500

    taller 1 javascript basico - maicol grajales

---
+++ constantes.js
@@ -0,0 +1,4 @@
+const nombre = "maicol grajales"
+const edad = 20
+
+console.log("Hola, mi nombre es", nombre, "y tengo", edad, "años")
---
+++ variable.js
@@ -0,0 +1,12 @@
+let nombre = "Maicol Andres Grajales Lopez"
+console.log(nombre)
+
+let grupo = 1154
+console.log(grupo)
+
+
+let curso = "JavaScript Basico"
+console.log(curso)
+
+let clase = "definicion de variables y constantes"
+console.log(clase)
